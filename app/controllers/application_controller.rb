a3ac5433ecd35242151fdb7eac190a319de3ce89
harima-yuya <yuya0203.h@gmail.com>
2022-07-14 23:25:25 +0900

    宿泊アプリの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+def set_search
+  @search = Inn.ransack(params[:q])
+  @inns = @search.result
+end
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up,keys:[:name]) 
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'posts/index'
+  get "posts", to: "posts#index"
+  
+  
+
+  root 'posts#home'
+  get "posts/home"
+
+  get "posts/search"
+  post "posts/search"
+  
+  
+  get 'posts/add'
+  post "posts/add", to: "posts#create"
+
+  get 'posts/:id', to: "posts#show"
+ 
+  get 'posts/edit/:id', to: "posts#edit"
+  patch 'posts/edit/:id', to: "posts#edit"
+
+  get "posts/delete/:id", to: "posts#delete"
+
+  
+  
+  
 end
