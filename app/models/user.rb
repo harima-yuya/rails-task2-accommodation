52538ab48f619c2f16f8e99b17f0a8ba5debdcf8
harima-yuya <yuya0203.h@gmail.com>
2022-07-17 10:57:59 +0900

    バリテーションの画像必須と自己紹介必須を削除して、アカウント登録ができないエラーを修正。userのテーブルと画像や自己紹介のテーブルは分て作った方がよかったかもしれない

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
   mount_uploader :image, AvatarUploader
 
   validates :name,presence: {message: "名前は必須項目です"}
-  validates :image,presence: {message: "画像は必須項目です"}
-  validates :comment,presence: {message: "自己紹介は必須項目です"}
-
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
